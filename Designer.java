921692f199848de92d18c5babf3ec8cc1183c613
Andre <andre_lfa@outlook.com>
2021-08-11 16:27:29 -0300

    Atualização dos códigos pós aula

--- Designer.java
+++ Designer.java
@@ -1,3 +1,4 @@
+//
 public class Designer extends Funcionario {
 	
 	public double getBonificacao() {
--- Funcionario.java
+++ Funcionario.java
@@ -1,14 +1,12 @@
+//não pode instanciar dessa classe, pq é abstrata
 public abstract class Funcionario {
 
 	private String nome;
 	private String cpf;
 	private double salario;
 	
-	
-	public double getBonificacao() {
-		return this.salario * 0.05;
-	}
-	
+	//metodo sem corpo, não há implementação
+	public abstract double getBonificacao();
 	
 	public String getNome() {
 		return nome;
